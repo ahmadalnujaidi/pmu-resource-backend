2ecc46a83ef1abfd83b936f10257039ca5e42b55
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2025-03-14 02:27:54 +0300

    second admin acc

--- src/auth/guards/admin.guard.ts
+++ src/auth/guards/admin.guard.ts
@@ -12,10 +12,9 @@ export class AdminGuard implements CanActivate {
 
     // list of admins assigned to variables.
     const ADMIN_EMAIL_1 = configService.get<string>('ADMIN_EMAIL_1');
-    // const ADMIN_EMAIL_2 = configService.get<string>('ADMIN_EMAIL_2');
+    const ADMIN_EMAIL_2 = configService.get<string>('ADMIN_EMAIL_2');
 
     // check if the variable is an admin
-    return user && (user.email === ADMIN_EMAIL_1) // || user.email === ADMIN_EMAIL_2);
+    return user && (user.email === ADMIN_EMAIL_1  || user.email === ADMIN_EMAIL_2);
   }
-  
 } 
